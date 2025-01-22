4f30ec1550b33e22a195d1b745c6f1e052927f43
Max Novikoff <novikoff04@gmail.com>
2025-01-22 16:52:38 +0500

    refactor program for double instead of int

--- lab-01/Main.java
+++ lab-01/Main.java
@@ -7,13 +7,10 @@ public class Main {
 }
 
 class HornerPolynomial {
-    // Function that returns value of poly[0]x^(n-1) +
-    // poly[1]x^(n-2) + ... + poly[n-1]
-    static int horner(int poly[], int n, int x) {
-        // Initialize result
-        int result = poly[0];
+    // Метод Горнера для работы с double
+    static double horner(double[] poly, int n, double x) {
+        double result = poly[0];
 
-        // Evaluate value of polynomial using Horner's method
         for (int i = 1; i < n; i++) {
             result = result * x + poly[i];
         }
@@ -27,14 +24,14 @@ class HornerPolynomial {
         System.out.println("Enter the number of terms in the polynomial:");
         int n = sc.nextInt();
 
-        int[] poly = new int[n];
+        double[] poly = new double[n];
         System.out.println("Enter the coefficients of the polynomial starting from the highest degree:");
         for (int i = 0; i < n; i++) {
-            poly[i] = sc.nextInt();
+            poly[i] = sc.nextDouble();
         }
 
         System.out.println("Enter the value of x:");
-        int x = sc.nextInt();
+        double x = sc.nextDouble();
 
         System.out.println("Value of the polynomial is: " + horner(poly, n, x));
 
